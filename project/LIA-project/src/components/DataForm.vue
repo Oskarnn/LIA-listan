9c5a2499c7b262cc0d743485846ef58c5fe1f4d5
Oskarnn <112063723+Oskarnn@users.noreply.github.com>
2023-02-16 21:41:33 +0100

    Css updated

--- project/LIA-project/src/components/DataForm.vue
+++ project/LIA-project/src/components/DataForm.vue
@@ -107,7 +107,7 @@ export default {
 
 <template>
     <section class="form">
-     <div>
+     <div class="hero">
     <h3>Här kan jag lägga till och ändra företagen i listan</h3>
     <p>Vad vill jag göra?</p>
   </div>
@@ -138,45 +138,54 @@ export default {
     </form>
 
     <form class="put-form" v-else-if="check === 'PUT'">
-        <ul v-for="company in getCompanies">
-            <li>{{ company.id }} {{ company.name }}</li>
-        </ul>
-        <label>Vilket ID vill jag ändra på</label>
-        <input type="text" v-model="formData.putId">
-        <label>Företagets namn</label>
-        <input type="text" v-model="formData.name">
-        <label>Vilken typ av företag</label>
-        <input type="text" v-model="formData.info">
-        <label>Företagets plats</label>
-        <input type="text" v-model="formData.location">
-        <label>Inväntar svar</label>
-        <input type="radio" v-model="formData.status" value="Inväntar svar">
-        <label>Tackat nej</label>
-        <input type="radio" v-model="formData.status" value="Tackat nej">
-        <label>Kallad på intervju</label>
-        <input type="radio" v-model="formData.status" value="Kallad på intervju">
+        <div class="put-comp-list">
+            <ul v-for="company in getCompanies">
+                <li>{{ company.id }} {{ company.name }}</li>
+            </ul>
+        </div>
+        <div class="put-comp-form">
+            <label>Vilket ID vill jag ändra på</label>
+            <input type="text" v-model="formData.putId">
+            <label>Företagets namn</label>
+            <input type="text" v-model="formData.name">
+            <label>Vilken typ av företag</label>
+            <input type="text" v-model="formData.info">
+            <label>Företagets plats</label>
+            <input type="text" v-model="formData.location">
+            <label>Inväntar svar</label>
+            <input type="radio" v-model="formData.status" value="Inväntar svar">
+            <label>Tackat nej</label>
+            <input type="radio" v-model="formData.status" value="Tackat nej">
+            <label>Kallad på intervju</label>
+            <input type="radio" v-model="formData.status" value="Kallad på intervju">
 
-        <input type="submit" @click="formFunc('PUT')" value="Ändra">
-
+            <input type="submit" @click="formFunc('PUT')" value="Ändra">
+        </div>
 
     </form>
 
 
     <form v-else-if="check === 'DELETE'" class="delete-form">
-        <ul v-for="company in getCompanies">
-            <li>{{ company.id }} {{ company.name }}</li>
-        </ul>
+        <div class="delete-comp-list">
+            <ul v-for="company in getCompanies">
+                <li>{{ company.id }} {{ company.name }}</li>
+            </ul>
+        </div>
+        <div class="delete-comp-form">
         <label>Företagets ID</label>
         <input type="text" v-model="formData.putId">
 
-        <input v-if="showWarn === false" type="button" value="Ta bort" @click="showWarn = true">
-    </form>
+        <input v-if="showWarn === false" type="button" :disabled="formData.putId === null" value="Ta bort" @click="showWarn = true">
+
+
     <div v-if="showWarn"><p>Är du säker att du vill radera?</p>
 
 
     <input  type="button" value="Avbryt" @click="showWarn = false">
     <input type="button" value="Radera" @click="formFunc('DELETE')">
-</div>
+    </div>
+    </div>
+</form>
 
 
 </section>
@@ -186,6 +195,66 @@ export default {
 
 .form {
     height: 90vh;
+    margin-left: 5vw;
 }
+
+.hero > p {
+    font-weight: 700;
+    margin-top: 10vh;
+}
+
+
+#action {
+    display: flex;
+    flex-direction: row;
+
+}
+#action > label {
+    margin-left: 2vw;
+    margin-right: 1vw;
+
+}
+
+li{
+    list-style-type: none
+    ;
+}
+
+.post-form {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    width: 200px;
+    margin: 4vh 0 0 55vw;
+}
+.put-form {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin-top: 4vh;
+}
+
+.put-comp-form {
+    display: flex;
+    flex-direction: column;
+}
+
+.delete-form {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin-top: 4vh;
+}
+
+.delete-comp-form{
+    display: flex;
+    flex-direction: column;
+
+}
+.delete-comp-form > input{
+    margin-top: 3vh;
+
+}
+
 
 </style>
--- project/LIA-project/src/components/DataList.vue
+++ project/LIA-project/src/components/DataList.vue
@@ -35,9 +35,9 @@
 
 <template>
     <section :class="listClass">
-
+        <h3>Inväntar svar</h3>
         <div class="list" id="list-1">
-            <h3>Inväntar svar</h3>
+            <!-- <h3>Inväntar svar</h3> -->
             <ul v-for="company in companies">
                 <li v-if="company.status === 'Inväntar svar'">
                     <p>{{ company.name }}</p>
@@ -46,8 +46,9 @@
                 </li>
             </ul>
         </div>
+        <h3>Tackat nej</h3>
         <div class="list" id="list-2">
-            <h3>Tackat nej</h3>
+            <!-- <h3>Tackat nej</h3> -->
             <ul v-for="company in companies">
                 <li v-if="company.status === 'Tackat nej'"><p>{{ company.name }}</p>
                     <p>{{ company.info }}</p>
@@ -55,8 +56,9 @@
                 </li>
             </ul>
         </div>
+        <h3>Kallad på intervju</h3>
         <div class="list" id="list-3">
-            <h3>Kallad på intervju</h3>
+            <!-- <h3>Kallad på intervju</h3> -->
             <ul v-for="company in companies">
                 <li v-if="company.status === 'Kallad på intervju'"><p>{{ company.name }}</p>
                     <p>{{ company.info }}</p>
@@ -76,8 +78,8 @@
         display: flex;
         justify-content: space-evenly;
         align-items: flex-start;
-
-        margin-top: 5vh;
+        flex-direction: column;
+        margin-top: 4vh;
 
     }
 
@@ -87,56 +89,71 @@
     }
 
     li {
-        margin: 7vh;
-        border: 1px solid black;
-        border-radius: 20px;
-        width: 200px;
-        height: 150px;
+        width: 100vw;
+        display: flex;
+        justify-content: space-evenly;
+
 
 
     }
 
+
     li > p {
-        font-size: 1.3rem;
-        margin-top: 1.5vh;
+        font-size: 1rem;
+        margin-top: 3vh;
+
     }
     h3{
         margin-top: 4vh;
+        margin-left: 5vw;
     }
 
     .list {
-        min-width: 300px;
-        text-align: center;
-        border-radius: 20px;
+
+        border-radius: 5px;
+        display: flex;
+        flex-direction: column;
+        margin-left: 10vw
 
     }
 
-    #list-1{
+     #list-1{
         background-color: rgba(255,255,0, 0.2);
 
     }
+
+
+
+
     #list-2{
         background-color: rgba(239, 90, 90, 0.1);
     }
     #list-3{
-        background-color: rgba(118, 211, 136, 0.1);
+        background-color: rgba(66, 218, 96, 0.1);
+
+    }
+
+    p:nth-child(1) {
+        font-weight: 600;
     }
 
     #btn {
        display: flex;
        justify-content: center;
        align-items: center;
+       margin-top: 3vh;
     }
 
     button {
         text-decoration: none;
-        border-radius: 30px;
+        border-radius: 15px;
         background-color: white;
-        padding: 10px;
-        font-size: 1.2rem;
+        padding: 15px;
+        font-size: 1rem;
+        border: 1px solid black;
     }
 
     button:hover{
-        padding: 10px 15px;
+        padding: 15px 10px;
     }
 </style>
